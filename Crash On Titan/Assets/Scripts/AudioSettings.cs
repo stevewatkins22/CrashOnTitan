a3fcc5d071156a2792497910423e2562f43e2f9e
stevewatkins22 <32023712+stevewatkins22@users.noreply.github.com>
2021-04-16 21:30:21 +0100

    Multiple Assets and script changes
    Added Assets for background, platforms, enemy, player
    Animations for player, enemy
    script changes to player controller

---
+++ Crash On Titan/Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSettings : MonoBehaviour
+{
+
+    public AudioSource m_AudioSource;
+    public bool m_IsPlaying;
+    public float m_SliderValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_SliderValue = 0.5f;
+
+       m_AudioSource.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Toggle()
+    {
+        if(!m_IsPlaying)
+        {
+            m_IsPlaying = true;
+            m_AudioSource.mute = !m_AudioSource.mute;
+        }
+        else
+        {
+            m_IsPlaying = false;
+            m_AudioSource.mute = m_AudioSource.mute;
+        }
+    }
+}
---
+++ Crash On Titan/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private float movementInputDirection;
+
+    private int numberOfJumpsLeft;
+
+    private bool isFacingRight = true;
+    private bool isWalking;
+    private bool isGrounded;
+    private bool canJump;
+    private bool isJumping;
+
+    public float movementSpeed = 10;
+    public float jumpForce = 16;
+    public float groundCheckRadius;
+
+    public int numberOfJumps = 2;
+
+    public Transform groundCheck;
+
+    public LayerMask whatIsGround;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        numberOfJumpsLeft = numberOfJumps;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        CheckMovementDirection();
+        UpdateAnimations();
+        CheckSurroundings();
+        CheckIfCanJump();
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+ 
+    }
+
+    private void CheckIfCanJump()
+    {
+        if (isGrounded && rb.velocity.y <= 0)
+        {
+            numberOfJumpsLeft = numberOfJumps;
+            isJumping = false;
+        }
+        
+        if(numberOfJumpsLeft > 0)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
+
+    private void CheckMovementDirection()
+    {
+        if(isFacingRight && movementInputDirection < 0)
+        {
+            Flip();
+        }
+    
+    else if(!isFacingRight && movementInputDirection > 0)
+        {
+            Flip();
+        }
+
+        if(rb.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+    }
+
+    private void UpdateAnimations()
+    {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isJumping", isJumping);
+    }
+
+    private void CheckInput()
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal");
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void Jump()
+    {
+        if (canJump && numberOfJumpsLeft > 1)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            numberOfJumpsLeft--;
+            isJumping = true;
+        }
+        else if(canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            numberOfJumpsLeft--;
+            isJumping = true;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
